2c38d4d705cbda9298a55903c73275bd8bd036da
levi.cacau <levi.cacau@gmail.com>
2022-03-08 12:01:39 -0500

    Geraldo adapted for python3

---
+++ build/lib/geraldo/generators/__init__.py
@@ -0,0 +1,6 @@
+from geraldo.generators.html import HTMLGenerator
+from geraldo.generators.base import ReportGenerator
+from geraldo.generators.xmlstruct import XMLStructGenerator
+from geraldo.generators.pdf import PDFGenerator
+from geraldo.generators.text import TextGenerator
+from geraldo.generators.csvgen import CSVGenerator
